970f11d0abf22694151497c73a8487749dad7689
divibisoul <divibi2011@gmail.com>
2026-08-31 17:53:24 -0300

    feat(compute): add deterministic model catalog

--- compute/transcendental/models/model.go
+++ compute/transcendental/models/model.go
@@ -2,16 +2,33 @@ package models
 
 import (
 	"time"
-
 	"github.com/divibisoul/Orquestrador-/compute/transcendental/core"
 )
 
-type PerformanceModel interface {
-	Name() string
-	EstimateTime(wl core.Workload) time.Duration
-	GetPFLOPS(p core.Precision) float64
-	GetBandwidthGBs() float64
-	GetMemoryCapacityGB() int64
-	GetMaxParallelUnits() int
-	Supports(p core.Precision) bool
+type PerformanceModel = core.PerformanceModel
+
+type Catalog struct {
+	Blackwell BlackwellB200
+	VeraRubin VeraRubin
+	CDNA5 CDNA5
+	Trillium Trillium
+	Atlas Atlas950
 }
+
+func DefaultCatalog(cfg core.Config) []PerformanceModel {
+	return []PerformanceModel{
+		BlackwellB200{Efficiency:cfg.Simulation.EfficiencyFactor, Sparse:cfg.Simulation.UseSparsity},
+		VeraRubin{Efficiency:cfg.Simulation.EfficiencyFactor},
+		CDNA5{Efficiency:cfg.Simulation.EfficiencyFactor},
+		Trillium{Efficiency:cfg.Simulation.EfficiencyFactor},
+		Atlas950{Efficiency:cfg.Simulation.EfficiencyFactor, ScaleFactor:1},
+	}
+}
+
+var _ PerformanceModel = BlackwellB200{}
+var _ PerformanceModel = VeraRubin{}
+var _ PerformanceModel = CDNA5{}
+var _ PerformanceModel = Trillium{}
+var _ PerformanceModel = Atlas950{}
+
+var _ = time.Second
