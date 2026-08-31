8fffa82de34b55ad036d1d450c83a016c0c19635
divibisoul <divibi2011@gmail.com>
2026-08-31 18:24:21 -0300

    refactor(tce): remove dead catalog import

--- compute/transcendental/models/model.go
+++ compute/transcendental/models/model.go
@@ -1,27 +1,24 @@
 package models
 
-import (
-	"time"
-	"github.com/divibisoul/Orquestrador-/compute/transcendental/core"
-)
+import "github.com/divibisoul/Orquestrador-/compute/transcendental/core"
 
 type PerformanceModel = core.PerformanceModel
 
 type Catalog struct {
 	Blackwell BlackwellB200
 	VeraRubin VeraRubin
-	CDNA5 CDNA5
-	Trillium Trillium
-	Atlas Atlas950
+	CDNA5     CDNA5
+	Trillium  Trillium
+	Atlas     Atlas950
 }
 
 func DefaultCatalog(cfg core.Config) []PerformanceModel {
 	return []PerformanceModel{
-		BlackwellB200{Efficiency:cfg.Simulation.EfficiencyFactor, Sparse:cfg.Simulation.UseSparsity},
-		VeraRubin{Efficiency:cfg.Simulation.EfficiencyFactor},
-		CDNA5{Efficiency:cfg.Simulation.EfficiencyFactor},
-		Trillium{Efficiency:cfg.Simulation.EfficiencyFactor},
-		Atlas950{Efficiency:cfg.Simulation.EfficiencyFactor, ScaleFactor:1},
+		BlackwellB200{Efficiency: cfg.Simulation.EfficiencyFactor, Sparse: cfg.Simulation.UseSparsity},
+		VeraRubin{Efficiency: cfg.Simulation.EfficiencyFactor},
+		CDNA5{Efficiency: cfg.Simulation.EfficiencyFactor},
+		Trillium{Efficiency: cfg.Simulation.EfficiencyFactor},
+		Atlas950{Efficiency: cfg.Simulation.EfficiencyFactor, ScaleFactor: 1},
 	}
 }
 
@@ -30,5 +27,3 @@ var _ PerformanceModel = VeraRubin{}
 var _ PerformanceModel = CDNA5{}
 var _ PerformanceModel = Trillium{}
 var _ PerformanceModel = Atlas950{}
-
-var _ = time.Second
