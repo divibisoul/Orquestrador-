b9894f4c6b502c3150da602e0230aee3c1b61d41
divibisoul <divibi2011@gmail.com>
2026-08-31 04:21:42 -0300

    feat: add provider abstraction for experimental Gemini adapter

---
+++ core/superagi/generator/providers/base.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class BaseProvider(ABC):
+    """Common interface for SuperAGI generation providers."""
+
+    @abstractmethod
+    def generate(self, prompt: str, **kwargs: Any) -> str:
+        raise NotImplementedError
