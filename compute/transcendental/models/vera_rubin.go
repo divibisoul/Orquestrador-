1d19509036bd5a0eeec155a3d8ae2b1173e646da
divibisoul <divibi2011@gmail.com>
2026-08-31 17:55:46 -0300

    fix(compute): align Vera Rubin support with supplied reference data

--- compute/transcendental/models/vera_rubin.go
+++ compute/transcendental/models/vera_rubin.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"time"
-
 	"github.com/divibisoul/Orquestrador-/compute/transcendental/core"
 )
 
@@ -12,6 +11,6 @@ func (m VeraRubin) Name() string { return "NVIDIA Vera Rubin (preliminary 2026)"
 func (m VeraRubin) GetBandwidthGBs() float64 { return 22000 }
 func (m VeraRubin) GetMemoryCapacityGB() int64 { return 288 }
 func (m VeraRubin) GetMaxParallelUnits() int { return 8192 }
-func (m VeraRubin) Supports(p core.Precision) bool { return p==core.FP4||p==core.FP8||p==core.FP16||p==core.BF16||p==core.FP32||p==core.FP64 }
-func (m VeraRubin) GetPFLOPS(p core.Precision) float64 { return map[core.Precision]float64{core.FP4:50,core.FP8:25,core.FP16:11,core.BF16:11,core.FP32:0,core.FP64:0}[p] }
+func (m VeraRubin) Supports(p core.Precision) bool { return p==core.FP4||p==core.FP8||p==core.FP16||p==core.BF16 }
+func (m VeraRubin) GetPFLOPS(p core.Precision) float64 { return map[core.Precision]float64{core.FP4:50,core.FP8:25,core.FP16:11,core.BF16:11}[p] }
 func (m VeraRubin) EstimateTime(wl core.Workload) time.Duration { return estimateRoofline(wl,m,eff(m.Efficiency)) }
