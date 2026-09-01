af186949c6e1a6ab92c12173f41fc742921998f4
divibisoul <divibi2011@gmail.com>
2026-09-01 01:04:25 -0300

    refactor(n07): make tracing runtime-local

--- observability/tracing.go
+++ observability/tracing.go
@@ -1,7 +1,17 @@
 package observability
 
-import("context";"go.opentelemetry.io/otel";"go.opentelemetry.io/otel/attribute";"go.opentelemetry.io/otel/codes";"go.opentelemetry.io/otel/trace")
+import "context"
 
-var tracer=otel.Tracer("soul.n07")
-func Start(ctx context.Context,operation,traceID string,payloadSize int)(context.Context,trace.Span){if ctx==nil{ctx=context.Background()};ctx,span:=tracer.Start(ctx,operation);span.SetAttributes(attribute.String("soul.nucleus","N07"),attribute.String("operation",operation),attribute.String("soul.trace_id",traceID),attribute.Int("payload.size",payloadSize));return ctx,span}
-func End(span trace.Span,err error){if span==nil{return};if err!=nil{span.RecordError(err);span.SetStatus(codes.Error,err.Error())}else{span.SetStatus(codes.Ok,"")};span.End()}
+type Span struct {
+	operation string
+	traceID   string
+}
+
+func Start(ctx context.Context, operation, traceID string, _ int) (context.Context, *Span) {
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	return ctx, &Span{operation: operation, traceID: traceID}
+}
+
+func End(_ *Span, _ error) {}
