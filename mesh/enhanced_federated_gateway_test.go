724891be0f0f1fab99277c823740e0ef96c65163
divibisoul <divibi2011@gmail.com>
2026-09-01 10:55:11 -0300

    test(n07): cover canonical parallel federation gateway construction

---
+++ mesh/enhanced_federated_gateway_test.go
@@ -0,0 +1,5 @@
+package mesh
+
+import("testing";"github.com/divibisoul/Orquestrador-/neural";"github.com/divibisoul/Orquestrador-/orchestrator";"github.com/divibisoul/Orquestrador-/prefrontal";"github.com/divibisoul/Orquestrador-/supergpu")
+
+func TestEnhancedFederatedGatewayConstruction(t *testing.T){n,_:=neural.New(2,.1);c,_:=prefrontal.New(.1,4);g:=supergpu.New(nil);g.Discover();e,err:=orchestrator.New(n,c,g);if err!=nil{t.Fatal(err)};gw:=NewEnhancedFederatedHTTPGateway(e);if gw==nil||gw.base==nil{t.Fatal("enhanced gateway not initialized")};if len(e.Operations())<6{t.Fatal("built-in operations missing")};_ = e.Shutdown(nil)}
