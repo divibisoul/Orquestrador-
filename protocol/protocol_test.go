7fb699fc2d06895e4beef7bf61f35d3cb9e01a34
divibisoul <divibi2011@gmail.com>
2026-09-01 00:06:40 -0300

    test(n07): verify shared protocol

---
+++ protocol/protocol_test.go
@@ -0,0 +1,4 @@
+package protocol
+
+import("testing";"time")
+func TestProtocol(t *testing.T){m:=NewMessage("N01","N07","command","compute.execute",[]float64{1,2});b,err:=Encode(m);if err!=nil{t.Fatal(err)};d,err:=Decode(b);if err!=nil{t.Fatal(err)};if d.TraceID!=m.TraceID||d.Version!=Version{t.Fatal("protocol roundtrip failed")};p:=Propagate(m,"N07","N02","reply",[]float64{3});if p.TraceID!=m.TraceID||p.Sequence!=m.Sequence+1{t.Fatal("trace propagation failed")};m.Deadline=time.Now().Add(-time.Second);if err:=m.Validate();err==nil{t.Fatal("expired deadline accepted")}}
