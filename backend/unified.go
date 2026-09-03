56b322acc717c9f5248056a8664232074521230d
divibisoul <divibi2011@gmail.com>
2026-09-03 02:55:47 -0300

    refactor(n07): remove silent capability-registration errors from backend bootstrap

--- backend/unified.go
+++ backend/unified.go
@@ -3,8 +3,5 @@ package backend
 import "github.com/divibisoul/Orquestrador-/orchestrator"
 
 func NewUnified(engine *orchestrator.Engine, cfg Config) *Server {
-	s := New(engine, cfg)
-	_ = orchestrator.RegisterSuperGPUOperations(engine)
-	_ = orchestrator.RegisterAdvancedOperations(engine)
-	return s
+	return New(engine, cfg)
 }
